d8c06368ab58a78b22db7f8a4a3d4abfcbf8703a
Adel Sadek <adelabodoma@gmail.com>
2021-05-29 01:32:02 +0200

    feat: add statistics section

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+jQuery(document).ready(function ($) {
+    var swiper = new Swiper(".header__swiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+          el: ".header__swiper .swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".header__swiper .swiper-button-next",
+          prevEl: ".header__swiper .swiper-button-prev",
+        },
+      });
+
+      var statisticsSwiper = new Swiper(".statistics__swiper", {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        navigation: {
+          nextEl: ".statistics__swiper .swiper-button-next",
+          prevEl: ".statistics__swiper .swiper-button-prev",
+        },
+      });
+});    
+
