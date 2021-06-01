d81407d310c6617eeefbc90a48a6636fb28aaac5
Adel Sadek <adelabodoma@gmail.com>
2021-06-02 01:27:02 +0200

    feat: responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,65 +1,130 @@
 jQuery(document).ready(function ($) {
-    const swiper = new Swiper(".header__swiper", {
-        slidesPerView: 1,
-        spaceBetween: 30,
-        loop: true,
-        pagination: {
-          el: ".header__swiper .swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".header__swiper .swiper-button-next",
-          prevEl: ".header__swiper .swiper-button-prev",
-        },
-      });
+  const swiper = new Swiper(".header__swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".header__swiper .swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".header__swiper .swiper-button-next",
+      prevEl: ".header__swiper .swiper-button-prev",
+    },
+  });
 
-      const statisticsSwiper = new Swiper(".statistics__swiper", {
-        slidesPerView: 4,
-        spaceBetween: 30,
-        navigation: {
-          nextEl: ".statistics__swiper .swiper-button-next",
-          prevEl: ".statistics__swiper .swiper-button-prev",
-        },
-      });
+  const statisticsSwiper = new Swiper(".statistics__swiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".statistics__swiper .swiper-button-next",
+      prevEl: ".statistics__swiper .swiper-button-prev",
+    },
+  });
 
-      const ProgramSwiper = new Swiper(".program__swiper", {
-        slidesPerView: 4,
-        spaceBetween: 30,
-        navigation: {
-          nextEl: ".program__controls .swiper-button-next",
-          prevEl: ".program__controls .swiper-button-prev",
-        },
-      });
-
-      const activitiesSwiper = new Swiper(".activities__swiper", {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        navigation: {
-          nextEl: ".activities__controls .swiper-button-next",
-          prevEl: ".activities__controls .swiper-button-prev",
-        },
-      });
-
-      const testimonialSwiper = new Swiper(".testimonial__swiper", {
+  const ProgramSwiper = new Swiper(".program__swiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".program__controls .swiper-button-next",
+      prevEl: ".program__controls .swiper-button-prev",
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
         slidesPerView: 2,
-        spaceBetween: 30,
-        navigation: {
-          nextEl: ".testimonial__controls .swiper-button-next",
-          prevEl: ".testimonial__controls .swiper-button-prev",
-        },
-      });
-      
-      const twitterSwiper = new Swiper(".twitter-widget__swiper", {
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 40
+      },
+      // when window width is >= 640px
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 40
+      },
+      991: {
+        slidesPerView: 4,
+        spaceBetween: 20
+      }
+    }
+  });
+
+  const activitiesSwiper = new Swiper(".activities__swiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".activities__controls .swiper-button-next",
+      prevEl: ".activities__controls .swiper-button-prev",
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
         slidesPerView: 1,
-        spaceBetween: 30,
-        pagination: {
-          el: ".twitter-widget__controls .swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".twitter-widget__controls .swiper-button-next",
-          prevEl: ".twitter-widget__controls .swiper-button-prev",
-        },
-      });
-});    
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      // when window width is >= 768px
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      // when window width is >= 991px
+      991: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      },
+    }
+  });
+
+  const testimonialSwiper = new Swiper(".testimonial__swiper", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".testimonial__controls .swiper-button-next",
+      prevEl: ".testimonial__controls .swiper-button-prev",
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      // when window width is >= 768px
+      1200: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+    }
+  });
+
+  const twitterSwiper = new Swiper(".twitter-widget__swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: ".twitter-widget__controls .swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".twitter-widget__controls .swiper-button-next",
+      prevEl: ".twitter-widget__controls .swiper-button-prev",
+    },
+  });
+});
 
